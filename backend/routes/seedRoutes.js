9495f8a2a31002c01342bac549018490b3379bdc
Long <laptoptandt@Longs-Macbookpro.local>
2023-06-12 14:01:31 +0700

    5th update

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -1,16 +1,22 @@
 import express from 'express'
 import Product from '../models/productModel.js'
 import data from '../data.js'
+import User from '../models/userModel.js';
 
 const seedRouter = express.Router()
 seedRouter.get('/', async (req, res) => {
     try {
         await Product.deleteMany({})
+        await User.deleteMany({})
         const createdProduct = await Product.insertMany(data.products)
-        res.send({ createdProduct })
+        const createdUsers = await User.insertMany(data.users)
+        res.send({ createdProduct, createdUsers })
+
     } catch (error) {
         console.log(error);
         res.status(500).send({ message: 'Internal Server Error' });
     }
+
+
 });
 export default seedRouter
